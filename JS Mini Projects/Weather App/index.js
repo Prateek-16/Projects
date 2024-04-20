84945dcdd444668de594a2bf425ab6dd1894e7d8
Prateek-16 <prateekverma54321@gmail.com>
2024-04-20 14:35:07 +0530

    commit-4

--- JS Mini Projects/Weather App/index.js
+++ JS Mini Projects/Weather App/index.js
@@ -9,7 +9,7 @@ const userInfoContainer = document.querySelector(".user-info-container");
 
 //initially variables needed
 let currentTab = userTab;
-const API_KEY = "168771779c71f3d64106d8a88376808a";
+const API_KEY = "d1845658f92b31c64bd94f06f7188c9c";
 
 currentTab.classList.add("current-tab");
 getfromSessionStorage();
@@ -63,7 +63,7 @@ async function fetchUserWeatherInfo(cooridinates){
     //API CALL
     try {
         const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`);
-        const data = await response.JSON();
+        const data = await response.json();
         loadingScreen.classList.remove("active");
         userInfoContainer.classList.add("active");
         renderWeatherInfo(data);
@@ -84,16 +84,19 @@ function renderWeatherInfo(weatherInfo){
     const temp = document.querySelector("[data-temp]");
     const windspeed = document.querySelector("[data-windspeed]");
     const humidity = document.querySelector("[data-humidity]");
-    const clouds = document.querySelector("[data-cloudiness]");
+    const cloudiness = document.querySelector("[data-cloudiness]");
 
+    console.log(weatherInfo);
+
+    //fetch values from weatherINfo object and put it UI elements
     cityName.innerText = weatherInfo?.name;
-    countryFlag.src = `https://flagcdn.com/144x108/${weatherInfo?.sys?.country.toLowerCase()}.png`;
-    description.innerText = weatherInfo?.weather?.[0]?.description;
+    countryIcon.src = `https://flagcdn.com/144x108/${weatherInfo?.sys?.country.toLowerCase()}.png`;
+    desc.innerText = weatherInfo?.weather?.[0]?.description;
     weatherIcon.src = `http://openweathermap.org/img/w/${weatherInfo?.weather?.[0]?.icon}.png`;
-    temp.innerText = `${weatherInfo?.main?.temp.toFixed(2)} °C`;
-    windspeed.innerText = `${weatherInfo?.wind?.speed.toFixed(2)} m/s`;
-    humidity.innerText = `${weatherInfo?.main?.humidity.toFixed(2)} %`;
-    clouds.innerText = `${weatherInfo?.clouds?.all.toFixed(2)} %`;
+    temp.innerText = `${weatherInfo?.main?.temp} °C`;
+    windspeed.innerText = `${weatherInfo?.wind?.speed} m/s`;
+    humidity.innerText = `${weatherInfo?.main?.humidity}%`;
+    cloudiness.innerText = `${weatherInfo?.clouds?.all}%`;
 
 };
 
@@ -111,7 +114,7 @@ function showPosition(position){
         lat: position.coords.latitude,
         lon: position.coords.longitude,
     }
-    sessionStorage.setItem("userCoordinates", JSON.stringify(userCoordinates));
+    sessionStorage.setItem("user-coordinates", JSON.stringify(userCoordinates));
     fetchUserWeatherInfo(userCoordinates);
 
 };
@@ -121,24 +124,21 @@ grantAccessButton.addEventListener("click",getLocation);
 
 let searchInput = document.querySelector("[data-searchInput]");
 searchForm.addEventListener("submit",(e) =>{
-    e.preventDefault();
+    e.preventDefault();  // this would case page reload
     if(searchInput.value ==="") return ;
-    fetchSearchWeatherInfo(searchInput.value);
+    fetchSearchWeatherInfo(searchInput.value);  //function for only city name
 });
 
 async function fetchSearchWeatherInfo(city) {
-    loadingContainer.classList.add("active");
+    loadingScreen.classList.add("active");
     userInfoContainer.classList.remove("active");
     grantAccessContainer.classList.remove("active");
-    notFound.classList.remove("active");
+
     try {
         const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`);
 
-        const data = await response.JSON();
-        if (!data.sys) {
-            throw data;
-        }
-        loadingContainer.classList.remove('active');
+        const data = await response.json();
+        loadingScreen.classList.remove('active');
         userInfoContainer.classList.add('active');
         renderWeatherInfo(data);
     }
